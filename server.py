fc84491a422dee16e0ad0a295e2738229e126e56
Logan Newhall <logannewhall98@gmail.com>
2022-05-30 21:32:36 -0600

    AWS debugging

--- server.py
+++ server.py
@@ -2,4 +2,4 @@ from flask_app import app
 from flask_app.controllers import users, games, game_has_users, friends, friend_requests
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True, port=5000)
